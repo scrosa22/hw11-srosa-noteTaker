5b8898b715adf83b244b60f29e25958b83f64807
steven rosa <scrosa13@outlook.com>
2022-09-18 18:50:01 -0400

    updating for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 // const notesDatabase = require("./db/db.json");
 const app = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 3001;
 
 // Middleware
 app.use(express.json());
