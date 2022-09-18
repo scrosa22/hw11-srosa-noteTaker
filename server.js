e664492e2a422152fe9c69b50033c691cfff91a3
steven rosa <scrosa13@outlook.com>
2022-09-18 18:09:07 -0400

    updating delete call, add images for readme

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { json } = require("express");
 
+const notesDatabase = require("./db/db.json");
 const app = express();
 
 const PORT = process.env.port || 3001;
@@ -14,12 +14,12 @@ app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 // GET Route for notes page
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 app.get("/api/notes", (req, res) =>
@@ -35,16 +35,45 @@ app.post("/api/notes", function (req, res) {
   let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   let newNote = req.body;
   let userID = savedNote.length.toString();
+
   newNote.id = userID;
   savedNote.push(newNote);
 
-  fs.writeFileSync('./db/db.json', JSON.stringify(savedNote))
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNote));
+    
+  
+  
+    res.json({
+        message:'note created',
+        status: true
+    })
 
-  console.log("note saved", savedNote);
 
-  res.json(savedNote);
+  console.log("note saved", newNote);
+
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+//   let noteToDelete = notesDatabase.find(x => x.id == req.params.id);
+
+//   notesDatabase = notesDatabase.filter(x => x.id != req.params.id);
+
+//   //Output
+//   noteToDelete
+//     ? res.json({
+//         message: `note was deleted successfully..`,
+//         status: true,
+//       })
+//     : res.json({
+//         message: `note was not found..`,
+//         status: false,
+//       });
+
+let savedNote = JSON.parse(fs.readFileSync("./db/db.json"));
+res.json(savedNote[Number(req.params.id)])
+
 });
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} ;)`)
+  console.log(`App listening at http://localhost:${PORT} `)
 );
