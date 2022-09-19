4785c2a84ec56b2992e20e508e0f57102651f260
steven rosa <scrosa13@outlook.com>
2022-09-19 13:32:33 -0400

    updating for heroku

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // GET Route for homepage
-// app.get("/", (req, res) =>
-//   res.sendFile(path.join(__dirname, "./public/index.html"))
-// );
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
-// // GET Route for notes page
-// app.get("/notes", (req, res) =>
-//   res.sendFile(path.join(__dirname, "./public/notes.html"))
-// );
+// GET Route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
 
 app.get("/api/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/db/db.json"))
